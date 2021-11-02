f535615ad58eb68fca1e654c3d6d2b18195b73fc
Ivaan2 <i.morales.mellado@iescristobaldemonroy.es>
2021-11-02 14:02:55 +0100

    ya sale la Act11

--- src/gestionFicheros/Actividad11.java
+++ src/gestionFicheros/Actividad11.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 public class Actividad11 {
     public static void main(String[] args) {
-        String ruta = "gestionFicheros.Actividad10";
+        String ruta = ruta = Actividad10.class.getCanonicalName();
+        String classpath = Actividad10.class.getClassLoader().getResource("").getPath();
         List<String> cmd = new ArrayList<>();
         cmd.add("java");
         cmd.add(ruta);
         ProcessBuilder pb = new ProcessBuilder(cmd);
+
         System.out.println("-------------- " + Actividad10.class.getCanonicalName());
-        System.out.println("-------------- " + Actividad10.class.getResource("").getPath());
-        String classpath = "C:/Users/Alumno/IdeaProjects/PSPRO-WS/out/production/PSPRO-WS/gestionFicheros/";
+
+
 
         pb.environment().put("CLASSPATH", classpath);
         pb.inheritIO();
