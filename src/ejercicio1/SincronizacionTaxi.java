d1172a969eb9592b4e9804b7a4c0883b2e1111f4
Ivaan2 <i.morales.mellado@iescristobaldemonroy.es>
2021-12-03 09:41:43 +0100

    Taxi finalizado

---
+++ src/ejercicio1/SincronizacionTaxi.java
@@ -0,0 +1,33 @@
+package ejercicio1; /**
+ * 
+ */
+
+/**
+ * @author JESUS
+ *
+ */
+public class SincronizacionTaxi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Taxi taxi = new Taxi();
+		
+		Runnable javi = new Cliente ("Javier", 15, taxi);
+		Runnable maria = new Cliente ("Mar�a", 8, taxi);
+		Runnable antonio = new Cliente("Antonio", 9, taxi);
+		Runnable luisa = new Cliente ("Luisa", 10, taxi);
+		
+		Thread tJavi = new Thread(javi);
+		Thread tMaria = new Thread(maria);
+		Thread tAntonio = new Thread(antonio);
+		Thread tLuisa = new Thread(luisa);
+		
+		tJavi.start();
+		tMaria.start();
+		tAntonio.start();
+		tLuisa.start();	
+	}
+
+}
---
+++ src/ejercicio1/Taxi.java
@@ -0,0 +1,83 @@
+package ejercicio1; /**
+ * 
+ */
+
+/**
+ * @author JESUS
+ *
+ */
+public class Taxi {
+	private double precioKmCarrera;
+	private double importeCarrera;
+	private int numKmCarrera;
+	private boolean disponible;
+
+	public Taxi(){
+		precioKmCarrera = 2.5;
+		importeCarrera = 0;
+		numKmCarrera = 0;
+		disponible = true;
+	}
+
+	public double getPrecioKmCarrera() {
+		return precioKmCarrera;
+	}
+
+	public void setPrecioKmCarrera(double precioKmCarrera) {
+		this.precioKmCarrera = precioKmCarrera;
+	}
+
+	public double getImporteCarrera() {
+		return importeCarrera;
+	}
+
+	public void setImporteCarrera(double importeCarrera) {
+		this.importeCarrera = importeCarrera;
+	}
+
+	public int getNumKmCarrera() {
+		return numKmCarrera;
+	}
+
+	public void setNumKmCarrera(int numKmCarrera) {
+		this.numKmCarrera = numKmCarrera;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	public synchronized void iniciarTrayecto(Cliente c) {
+		while(!disponible){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+		System.out.println("Se ha subido " + c.getNombreCliente());
+		setNumKmCarrera(this.getNumKmCarrera());
+		setImporteCarrera(c.getNumKmCarrera() * this.precioKmCarrera);
+
+		setDisponible(false);
+		notifyAll();
+	}
+
+	public synchronized void finalizarTrayecto(Cliente c){
+		while(disponible){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		setDisponible(true);
+		System.out.println("El importe del trayecto de " + c.getNombreCliente() + " es: " + getImporteCarrera());
+		notifyAll();
+	}
+}
